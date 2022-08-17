8bca6b3b9dc3e3718060126e2e9041716e771778
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-17 18:19:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers = ['James','Lilian','Edwin','Alex','Lemuel'];
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers =function(drivers){
+    return drivers.slice (-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier =function(fareMultiplier){
+    return function(value){
+        return fareMultiplier * value
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+
+const  fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(arrayOfDrivers)
+}
+function selectDifferentDrivers(arrayOfDrivers,returnLastTwoDrivers){
+    return returnLastTwoDrivers(arrayOfDrivers)
+}
